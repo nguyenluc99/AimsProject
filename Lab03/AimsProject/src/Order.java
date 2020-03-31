35e7e8d39eacc4d6cb9b22e05ffde69a017c5287
nguyenluc99 <yeuma99@gmail.com>
2020-03-31 22:56:39 +0700

    add Lab03

---
+++ Lab03/AimsProject/src/Order.java
@@ -0,0 +1,18 @@
+// package src;
+public class Order {
+    public static final int MAX_NUMBERS_ORDERED = 10;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int numOfOrder = 0;
+    private float cost = 0;
+    public static void main(String[] args) {
+
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd) {
+        this.itemOrdered[this.numOfOrder] = dvd;
+        this.numOfOrder ++;
+        this.cost += dvd.getCost();
+    }
+    public float totalCost() {
+        return this.cost;
+    }
+}
---
+++ Lab03/DigitalVideoDisc.java
@@ -0,0 +1,46 @@
+package Lab03;
+
+public class DigitalVideoDisc{
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getCategory() {
+        return this.category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+
+}
